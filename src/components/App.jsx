ad298b9194c33403831b5f242faed7a1e5dd7b49
Suriyaa <76957372+suriyaaap13@users.noreply.github.com>
2022-04-13 17:19:10 +0530

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [content, setContent] = useState([
+    {
+      title: "",
+      note: ""
+    }
+  ]);
+  function addNewContent(obj) {
+    console.log(obj);
+    setContent((prevValue) => {
+      console.log(prevValue);
+      return [...prevValue, obj];
+    });
+  }
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea onAdd={addNewContent} />
       <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [title, setTitle] = useState("");
+  const [note, setNote] = useState("");
+  function handleSubmit(event){
+    event.preventDefault();
+    const obj = {title: title, note: note};
+    setTitle("");
+    setNote("");
+    return props.onAdd(obj);
+  }
+  function handleTitleChange(event){
+    setTitle(event.target.value);
+  }
+  function handleNoteChange(event){
+    setNote(event.target.value);
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+      <form onSubmit = {handleSubmit}>
+        <input onChange = {handleTitleChange} name="title" placeholder="Title" />
+        <textarea onChange = {handleNoteChange} name="content" placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
     </div>
